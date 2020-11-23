7a5c1208855563729269911e17180cfa31ac75f6
evilmao <faily0117@gmail>
2020-11-23 12:14:19 +0800

    [update] update balanceCheck email format

--- wallet-withdraw/transfer/checker/checker/balanceChecker.go
+++ wallet-withdraw/transfer/checker/checker/balanceChecker.go
@@ -15,9 +15,9 @@ import (
 
 var (
 	headContent                     = "【Balance Not Enough】 "
-	content                         = "%s balance(%s) less than minimum remain balance(%s),\n"
+	content                         = "%s balance(%s) less than minimum remain balance(%s),\n\t\t\t\t"
 	tailContent                     = "pls deposit ASAP."
-	checkTaskInterval time.Duration = 5
+	checkTaskInterval time.Duration = 2
 )
 
 type BalanceChecker struct {
@@ -42,8 +42,6 @@ func (c *BalanceChecker) Init(cfg *config.Config) {
 }
 
 func (c *BalanceChecker) Check() error {
-	log.Warnf("BalanceChecker start time:%s", c.lastBalanceCheckerTime.String())
-	log.Warnf("BalanceChecker timeNow:%s", time.Now().String())
 
 	if time.Now().Sub(c.lastBalanceCheckerTime) < time.Minute*checkTaskInterval {
 		return nil
@@ -55,7 +53,7 @@ func (c *BalanceChecker) Check() error {
 		minBalance = decimal.NewFromFloat(c.cfg.MinAccountRemain)
 	)
 
-	log.Infof("%s process...", c.Name())
+	log.Infof("%s task process...", c.Name())
 	c.lastBalanceCheckerTime = time.Now()
 	if currency == "" || minBalance.LessThan(decimal.Zero) {
 		err := fmt.Errorf("main currency or MinAccountRemain set wrong, check `currency` and `minAccountRemain` fields ")
