ddc07c40ce447761be4eeaf43344bc6176a41843
evilmao <faily0117@gmail>
2020-12-02 11:56:27 +0800

    [fix] fix  circle imports err

--- wallet-deposit/cmd/esyncd/run.go
+++ wallet-deposit/cmd/esyncd/run.go
@@ -1,4 +1,4 @@
-package esyncd
+package cmd
 
 import (
 	"strings"
--- wallet-deposit/cmd/imports/import.go
+++ wallet-deposit/cmd/imports/import.go
@@ -1,5 +1,8 @@
 package imports
 
 import (
+	_ "upex-wallet/wallet-deposit/cmd/bsyncd"
+	_ "upex-wallet/wallet-deposit/cmd/esyncd"
+
 	_ "upex-wallet/wallet-deposit/rpc/eth"
 )
