75346ea3279b59c4c7d194a5dd73d7c43b1c453c
evilmao <faily0117@gmail>
2020-12-03 18:06:40 +0800

    [fix] fix import package

--- wallet-deposit/syncer/bitcoin/gbtc/script.go
+++ wallet-deposit/syncer/bitcoin/gbtc/script.go
@@ -3,18 +3,15 @@ package gbtc
 import (
 	"bytes"
 
-	"github.com/fb996de/wallet-base/util"
-	"github.com/fb996de/wallet-tools/base/crypto"
+	"upex-wallet/wallet-base/util"
+	"upex-wallet/wallet-tools/base/crypto"
 )
 
 const (
-	OP_DUP = 0x76
-
-	OP_EQUALVERIFY = 0x88
-
-	OP_HASH160 = 0xa9
-
-	OP_CHECKSIG = 0xac
+	OpDup          = 0x76
+	OpEqualCVerify = 0x88
+	OpHash160      = 0xa9
+	OpCheckSig     = 0xac
 )
 
 func CreateP2PKHScript(address string, prefixLen uint8) []byte {
@@ -25,10 +22,10 @@ func CreateP2PKHScript(address string, prefixLen uint8) []byte {
 	_, h := crypto.DeBase58Check(address, prefixLen, false)
 
 	buf := new(bytes.Buffer)
-	buf.WriteByte(OP_DUP)
-	buf.WriteByte(OP_HASH160)
-	util.WriteVarBytes(buf, h)
-	buf.WriteByte(OP_EQUALVERIFY)
-	buf.WriteByte(OP_CHECKSIG)
+	buf.WriteByte(OpDup)
+	buf.WriteByte(OpHash160)
+	_ = util.WriteVarBytes(buf, h)
+	buf.WriteByte(OpEqualCVerify)
+	buf.WriteByte(OpCheckSig)
 	return buf.Bytes()
 }
