8aa63465b07f9be0bed70ab14feef8f67b9dcfa2
maoyf <maoyf@anchnet.com>
2021-03-15 22:46:29 +0800

    [update] only trx for wallet-tools

--- wallet-tools/cmd/genwallet/keypair/builder/eth.go
+++ wallet-tools/cmd/genwallet/keypair/builder/eth.go
@@ -6,9 +6,10 @@ import (
 	"upex-wallet/wallet-tools/base/crypto/signer"
 )
 
-func init() {
-	registerETHLike("ETH", "ETC", "WAN", "DCAR", "SMT", "FT", "IONC")
-}
+// todo:隐藏 ETH
+// func init() {
+// 	registerETHLike("ETH", "ETC", "WAN", "DCAR", "SMT", "FT", "IONC")
+// }
 
 func registerETHLike(classes ...string) {
 	registerBatch(classes, key.NewSecp256k1, signer.NewSecp256k1Recoverable(false), addrprovider.NewETH())
